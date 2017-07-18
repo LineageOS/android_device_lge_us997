a77f271b7e6e680f3c9d05a6b8b5a41aa67328ff
rc420head <dennislebhardt@yahoo.com>
2017-07-17 21:04:54 -0500

    tmo > nao
    Change-Id: I212434e5891b8c33d8f0f7bc0c3b3ae77f4a6ad8

--- lineage.mk
+++ lineage.mk
@@ -10,6 +10,6 @@ PRODUCT_NAME := lineage_us997
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g6" \
-    PRODUCT_NAME="lucye_tmo_us" \
-    BUILD_FINGERPRINT="lge/lucye_tmo_us/lucye:7.0/NRD90U/17062223981e1:user/release-keys" \
-    PRIVATE_BUILD_DESC="lucye_tmo_us-user 7.0 NRD90U 17062223981e1 release-keys"
+    PRODUCT_NAME="lucye_nao_us" \
+    BUILD_FINGERPRINT="lge/lucye_nao_us/lucye:7.0/NRD90U/17062223981e1:user/release-keys" \
+    PRIVATE_BUILD_DESC="lucye_nao_us-user 7.0 NRD90U 17062223981e1 release-keys"
