6e0395209dc7539cac31bb9d844fb063a4f6590f
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-11 15:49:46 -0400

    us997: Move bluetooth to msm8996-common
    Change-Id: I3888a9bfcba769f1488c87cc5c4ba7375914c0cc

--- device.mk
+++ device.mk
@@ -22,10 +22,6 @@ $(call inherit-product-if-exists, vendor/lge/us997/us997-vendor.mk)
 # common g6
 $(call inherit-product, device/lge/g6-common/g6.mk)
 
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_lucye_nao_us.conf:system/etc/sensors/sensor_def_variable.conf
