e3bc855a3cb6ed76d32ade6d8d779b902f037e87
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-13 01:16:15 -0400

    us997: Update fingerprint from us997 v14a
    Change-Id: I4e3660a276ed40bcde79daa4369526c4f4c18bce

--- lineage.mk
+++ lineage.mk
@@ -11,5 +11,5 @@ PRODUCT_NAME := lineage_us997
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g6" \
     PRODUCT_NAME="lucye_nao_us" \
-    BUILD_FINGERPRINT="lge/lucye_nao_us/lucye:7.0/NRD90U/171711753c9cd:user/release-keys" \
-    PRIVATE_BUILD_DESC="lucye_nao_us-user 7.0 NRD90U 171711753c9cd release-keys"
+    BUILD_FINGERPRINT="lge/lucye_nao_us/lucye:7.0/NRD90U/172061549d102:user/release-keys" \
+    PRIVATE_BUILD_DESC="lucye_nao_us-user 7.0 NRD90U 172061549d102 release-keys"
