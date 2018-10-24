338cb4fd6272315c78708113477da31368006b3f
Bruno Martins <bgcngm@gmail.com>
2018-10-24 08:23:16 -0400

    us997: Consider lineage_us997 product via AOSP way
    Change-Id: Ifed5e5b642ba9b935a0e46504fd47709f40d364f

--- lineage_us997.mk
+++ lineage_us997.mk
@@ -18,12 +18,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Inherit from us997 device
 $(call inherit-product, device/lge/us997/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := us997
-PRODUCT_NAME := full_us997
+PRODUCT_NAME := lineage_us997
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-US997
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="lucye" \
+    PRODUCT_DEVICE="lucye" \
+    PRODUCT_NAME="lucye_nao_us" \
+    PRIVATE_BUILD_DESC="lucye_nao_us-user 7.0 NRD90U 172061549d102 release-keys"
+
+BUILD_FINGERPRINT := "lge/lucye_nao_us/lucye:7.0/NRD90U/172061549d102:user/release-keys"
