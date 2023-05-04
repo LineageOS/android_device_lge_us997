acfe8ff8a74a3e1583878eaf865053486783212f
Nolen Johnson <johnsonnolen@gmail.com>
2023-05-04 12:47:24 -0400

    us997: Extract from EOL build
    Change-Id: I0f94fb9bfed790b898626f1dc46c9a397ce4edeb

--- lineage_us997.mk
+++ lineage_us997.mk
@@ -35,6 +35,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lucye" \
     PRODUCT_DEVICE="lucye" \
     PRODUCT_NAME="lucye_nao_us" \
-    PRIVATE_BUILD_DESC="lucye_nao_us-user 8.0.0 OPR1.170623.032 181521705197f release-keys"
+    PRIVATE_BUILD_DESC="lucye_nao_us-user 8.0.0 OPR1.170623.032 190420940e75c release-keys"
 
-BUILD_FINGERPRINT := "lge/lucye_nao_us/lucye:8.0.0/OPR1.170623.032/181521705197f:user/release-keys"
+BUILD_FINGERPRINT := "lge/lucye_nao_us/lucye:8.0.0/OPR1.170623.032/190420940e75c:user/release-keys"
